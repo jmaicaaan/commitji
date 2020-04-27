d1101dfa246a05c3d3be378396819cb7628149d2
jmaicaaan <johnmichaelubas.santos@gmail.com>
2020-04-27 16:42:45 +0800

    ✨- feat: Updated Settings type to include `commitTypes`

--- packages/core/src/types/settings.ts
+++ packages/core/src/types/settings.ts
@@ -1,10 +1,12 @@
 import { App } from '../constants';
+import { CommitType } from './commitType';
 
 export type JiraWorkflowTransition = Record<'key' | 'label' | 'workflowTransitionName', string>;
 
 export type Settings = {
   workflow: App.BasicWorkflow | App.JiraWorkflow;
   format?: string;
+  commitTypes?: CommitType[],
   jira: {
     allowIssueKeyPrompt: false;
     allowWorkflowTransitionPrompt: false;
