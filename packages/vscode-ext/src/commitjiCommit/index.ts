fd7f6c896246208aefbb9f64eb4c9449c22a9228
jmaicaaan <johnmichaelubas.santos@gmail.com>
2020-05-01 17:29:23 +0800

    💎 style: Removed some extra spaces

--- packages/vscode-ext/src/commitjiCommit/index.ts
+++ packages/vscode-ext/src/commitjiCommit/index.ts
@@ -4,9 +4,9 @@ import { alerter } from './utils/alerter';
 export const commitjiCommit = async () => {
   const settings = getSettings();
   const runWorkflow = getWorkflow(settings.workflow);
-  
+
   try {
-    await runWorkflow(settings);  
+    await runWorkflow(settings);
   } catch (error) {
     alerter.error(error.message);
   }
