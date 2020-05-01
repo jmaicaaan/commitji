215b60c66d271bd179f4ea0c21e7f94a0f268713
jmaicaaan <johnmichaelubas.santos@gmail.com>
2020-05-01 17:28:58 +0800

    ✨ feat: Added issueKeyExtractionPattern typing in the Settings

--- packages/vscode-ext/src/commitjiCommit/types/Settings.ts
+++ packages/vscode-ext/src/commitjiCommit/types/Settings.ts
@@ -3,4 +3,5 @@ import { Settings as CommitjiSettings } from '@commitji/core';
 export type Settings = CommitjiSettings & {
   // Auto commit is currently available on VSCode extension
   autoCommit: boolean;
+  issueKeyExtractionPattern: string;
 };
