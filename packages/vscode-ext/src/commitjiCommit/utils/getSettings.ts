01f1b67ba010cab58fe2e1a2fa0f1ec82908c60d
jmaicaaan <johnmichaelubas.santos@gmail.com>
2022-03-27 14:21:53 +0800

    ✨ feat: Allow retrieving settings from package json

--- packages/vscode-ext/src/commitjiCommit/utils/getSettings.ts
+++ packages/vscode-ext/src/commitjiCommit/utils/getSettings.ts
@@ -1,4 +1,6 @@
 import { workspace } from 'vscode';
+import { resolve } from 'path';
+import { readFileSync } from 'fs';
 
 import { App } from '@commitji/core';
 
@@ -8,16 +10,40 @@ const clean = (settings: Settings): Settings => ({
   ...settings,
   // Strings will result to "" and that will not trigger the default values
   format: settings.format || undefined,
-  jira: {
-    ...settings.jira,
-    // Safely ensure that the default extraction pattern will always be there
-    // https://stackoverflow.com/questions/19322669/regular-expression-for-a-jira-identifier/30518972#30518972
-    issueKeyExtractionPattern: settings.jira.issueKeyExtractionPattern || '((?<!([A-Z]{2,10})-?)[A-Z]+-\d+)',
-  },
+  commitTypes: settings.commitTypes || [],
+  ...settings.jira && {
+    jira: {
+      ...settings.jira,
+      // Safely ensure that the default extraction pattern will always be there
+      // https://stackoverflow.com/questions/19322669/regular-expression-for-a-jira-identifier/30518972#30518972
+      issueKeyExtractionPattern: settings.jira.issueKeyExtractionPattern || '((?<!([A-Z]{2,10})-?)[A-Z]+-\d+)',
+    },
+  }
 });
 
 export const getSettings = () => {
+  const packageJSONSettings = getSettingsInPackageJSON();
+
+  if (packageJSONSettings) {
+    return clean(packageJSONSettings) as unknown as Settings;
+  }
+
   return clean(
     workspace.getConfiguration(App.Name) as unknown as Settings
   );
+};
+
+const getSettingsInPackageJSON = () => {
+  const path = [...workspace.workspaceFolders || []].shift()?.uri.path || '';
+
+  try {
+    const manifestPath = resolve(path, 'package.json');
+    const packageJSON = JSON.parse(readFileSync(manifestPath, {
+      encoding: 'utf8',
+    }));
+    const settings = packageJSON['commitji'];
+    return settings;
+  } catch (error) {
+    return;
+  }
 };
